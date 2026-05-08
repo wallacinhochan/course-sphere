7e57f7bbf0b3ab081177709dd752d868c3a91e9d
Wallace Leão <wgsl@cin.ufpe.br>
2026-05-07 22:38:55 -0300

    test: add model and request specs for User, Course, Lesson and Auth

--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email).case_insensitive }
+  it { should have_many(:created_courses) }
+
+  it 'é inválido com email mal formatado' do
+    user = build(:user, email: 'nao-é-email')
+    expect(user).not_to be_valid
+  end
 end
